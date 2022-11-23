ac116a67f38b2122712058bce56bf07e42a2bb4c
Alejandro Salas <asalas.wd@outlook.com>
2022-11-23 15:34:37 -0500

    Create TextPanel class overriding paintComponent method

---
+++ src/com/java21days/TextPanel.java
@@ -0,0 +1,12 @@
+package com.java21days;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class TextPanel extends JPanel {
+
+    @Override
+    public void paintComponent(Graphics comp){
+        Graphics2D comp2D = (Graphics2D) comp;
+    }
+}
