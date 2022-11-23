a3d85365613a222e72180c7408be9f14ebbd4e5c
Alejandro Salas <asalas.wd@outlook.com>
2022-11-23 16:38:02 -0500

    Align text to appear in the center

--- src/com/java21days/TextFrame.java
+++ src/com/java21days/TextFrame.java
@@ -4,13 +4,14 @@ import javax.swing.*;
 import java.awt.*;
 
 public class TextFrame extends JFrame {
-    Dimension dim = new Dimension(300, 150);
+    Dimension dim = new Dimension(425, 150);
     TextFrame(String title, String text){
         super(title);
         setSize(dim);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-
+        TextPanel panel = new TextPanel(text);
+        add(panel);
         setVisible(true);
     }
 }
--- src/com/java21days/TextPanel.java
+++ src/com/java21days/TextPanel.java
@@ -13,6 +13,15 @@ public class TextPanel extends JPanel {
 
     @Override
     public void paintComponent(Graphics comp){
+        super.paintComponent(comp);
         Graphics2D comp2D = (Graphics2D) comp;
+        comp2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        Font font = new Font("Arial Black", Font.BOLD, 14);
+        FontMetrics metrics = getFontMetrics(font);
+        comp2D.setFont(font);
+        int x = (getSize().width - metrics.stringWidth(this.text))/2;
+        int y = getSize().height/2;
+        comp2D.drawString(this.text, x, y);
+
     }
 }
